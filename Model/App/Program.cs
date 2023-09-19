67a67c79db0de36158bedb34ead59b6c5c6786cb
Farouk-Ahmed <140400450+Farouk-Ahmed@users.noreply.github.com>
2023-09-20 02:40:45 +0300

    Add files via upload
    Project

---
+++ Model/App/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Model;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Trainee> trainees = new List<Trainee>();
+
+            Trainee trainee1 = new Trainee(2, "Farouk");
+            Trainee trainee2 = new Trainee(1, "Omar");
+
+            int comparisonResult = trainee1.CompareTo(trainee2);
+
+            Console.WriteLine($"Comparison result: {comparisonResult}");
+            Console.WriteLine(trainee1);
+            Console.WriteLine(trainee2);
+
+
+            while (true)
+            {
+                Console.WriteLine("How many Trainees do you want to add (0 to Out Of App)? ");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num == 0)
+                    break;
+
+                for (int i = 0; i < num; i++)
+                {
+                    Console.WriteLine($"Enter Trainee {i} ID: ");
+                    int id = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine($"Enter Trainee {i} Name: ");
+                    string name = Console.ReadLine();
+
+                    Trainee trainee = new Trainee(id, name);
+                    trainees.Add(trainee);
+                }
+            }
+
+            trainees.Sort();
+
+            Console.WriteLine("Sorted Trainees by ID:");
+            foreach (var trainee in trainees)
+            {
+                Console.WriteLine(trainee.ToString());
+            }
+        }
+    }
+}
+
+
+
---
+++ Model/Model/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Model
+{
+    public class Trainee : IComparable<Trainee>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Trainee(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int CompareTo(Trainee other)
+        {
+            return Id.CompareTo(other.Id);
+        }
+
+        public override string ToString()
+        {
+            return $"Trainee(Id: {Id}, Name: {Name})";
+        }
+    }
+}
