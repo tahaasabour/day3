2e84cbbebe6f48bf2b2137c536ad6ecdd3f316ec
Ahmedhasseb <ahmedhasseb310@gmail.com>
2023-09-19 15:00:12 +0300

    AhmedHassib lab 3 Advanced

---
+++ Models/App/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Models
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please Enter Number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Trainee[] tran = new Trainee[number];
+
+            do
+            {
+               
+                for (int i = 0; i < number; i++)
+                {
+                    tran[i] = new Trainee();
+                   Console.WriteLine("please Enter Id");
+                    tran[i].id = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("please Enter name");
+                    tran[i].name = Console.ReadLine();
+
+                }
+
+                Console.WriteLine("Please Enter number agin");
+                number = Convert.ToInt32(Console.ReadLine());
+                Array.Resize(ref tran, number);
+               
+            } while (number > 0);
+
+           
+
+        }
+      
+    }
+}
