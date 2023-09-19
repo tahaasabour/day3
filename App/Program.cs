b7d23ccf8bdd136004d7893ea4f2d3d1cbd61eb9
nazem0 <mohamednazem09@gmail.com>
2023-09-19 14:12:05 +0300

    Nazem - Lab

---
+++ App/Program.cs
@@ -0,0 +1,71 @@
+﻿//using Models; //Implicitly Used :v
+using Models;
+using System;
+using System.Collections;
+
+namespace App
+{
+    public class Program
+    {
+        private static Trainee[] Trainees = new Trainee[0];
+        static void Main()
+        {
+            //Ask the user for how many *trainees* he/she want to add.
+
+            Console.WriteLine("How many trainees do you want to add ?");
+            int HowManyUsers = int.Parse(Console.ReadLine());
+            Array.Resize(ref Trainees, HowManyUsers);
+            for (int i = 0; i < HowManyUsers; i++)
+            {
+                Console.WriteLine($"Enter trainee number {i + 1} data as ID then Name");
+                Trainees[i] = new Trainee
+                {
+                    Id = int.Parse(Console.ReadLine()),
+                    Name = Console.ReadLine()
+                };
+            }
+            PrintTrainees();
+
+            AddMoreTrainees();
+        }
+
+        static void AddMoreTrainees()
+        {
+                Console.WriteLine("1- Add More Trainees?");
+                Console.WriteLine("2- End Program");
+                int EndProgram = int.Parse(Console.ReadLine());
+                switch (EndProgram)
+                {
+                    case 1:
+                        {
+                        Console.WriteLine("Add Trainee Information As ID Then Name");
+                        Array.Resize(ref Trainees, Trainees.Length + 1);
+                            Trainees[Trainees.Length - 1] = new Trainee
+                            {
+                                Id = int.Parse(Console.ReadLine()),
+                                Name = Console.ReadLine()
+                            };
+                            PrintTrainees();
+
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+                }
+            }
+        
+
+        static void PrintTrainees()
+        {
+
+            Array.Sort(Trainees);
+            foreach (Trainee t in Trainees)
+            {
+                Console.WriteLine(t);
+            }
+        }
+    }
+
+}
---
+++ Models/Trainee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Models
+{
+    public class Trainee : IComparable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Trainee T = obj as Trainee;
+            return this.Id.CompareTo(T.Id);
+        }
+
+        public override string ToString()
+        {
+            return $"ID ==> {Id}, Name ==> {Name}";
+        }
+    }
+}
