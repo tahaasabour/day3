9dcdf325ca5924bf4c32dfd3416e66f5563a4e12
Mahmoud <mahmoud.shallaly@gmail.com>
2023-09-19 15:18:32 +0300

    uploading Day3 Lab

---
+++ Models/App/Program.cs
@@ -0,0 +1,31 @@
+﻿using Models;
+using System;
+namespace App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many trainees do you want to add?");
+            int count = int.Parse(Console.ReadLine());
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Models/Models/Trainee .cs
@@ -0,0 +1,103 @@
+﻿namespace Models
+{
+    public class Trainee : IComparable;
+    {    public int Id { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Name}";
+        }
+     
+    }
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Trainee.cs in the Models project
+//using System;
+
+//namespace Models
+//{
+//    public class Trainee : IComparable<Trainee>
+//    {
+//        public int Id { get; set; }
+//        public string Name { get; set; }
+
+//        public Trainee(int id, string name)
+//        {
+//            Id = id;
+//            Name = name;
+//        }
+
+//        public override string ToString()
+//        {
+//            return $"Id: {Id}, Name: {Name}";
+//        }
+
+//        public int CompareTo(Trainee other)
+//        {
+//            return Id.CompareTo(other.Id);
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
