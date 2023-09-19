00a93f5ad8f3f003246db40330e1a998075b96d1
Mahmoud <mahmoud.shallaly@gmail.com>
2023-09-19 15:29:36 +0300

    Done

--- Models/Models/Trainee .cs
+++ Models/Models/Trainee .cs
@@ -1,103 +1,22 @@
-﻿namespace Models
+﻿using System;
+
+namespace Models
 {
-    public class Trainee : IComparable;
-    {    public int Id { get; set; }
+    public class Trainee : IComparable
+    {
+        public int Id { get; set; }
         public string Name { get; set; }
+
+        public int CompareTo(object? obj)
+        {
+            Trainee other = obj as Trainee;
+            return Id.CompareTo(other.Id);
+        }
 
         public override string ToString()
         {
-            return $"{Id}, {Name}";
+            return $"Id: {Id}, Name: {Name}";
         }
-     
     }
 }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Trainee.cs in the Models project
-//using System;
-
-//namespace Models
-//{
-//    public class Trainee : IComparable<Trainee>
-//    {
-//        public int Id { get; set; }
-//        public string Name { get; set; }
-
-//        public Trainee(int id, string name)
-//        {
-//            Id = id;
-//            Name = name;
-//        }
-
-//        public override string ToString()
-//        {
-//            return $"Id: {Id}, Name: {Name}";
-//        }
-
-//        public int CompareTo(Trainee other)
-//        {
-//            return Id.CompareTo(other.Id);
-//        }
-//    }
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   
