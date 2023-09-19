b230d7dd75bdb327739278fc7ccb738ba3ddd114
Abdelrhman Ahmed <47417146+ZoRoOt3@users.noreply.github.com>
2023-09-19 14:25:28 +0300

    Abdelrhman Ahmed ALi

---
+++ Models/App/TraineeExtender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace App
+{
+    internal class TraineeExtender : IComparer<Trainee>
+    {
+        public int Compare(Trainee? x, Trainee? y)
+        {
+            return x.CompareTo(y);
+        }
+    }
+}
